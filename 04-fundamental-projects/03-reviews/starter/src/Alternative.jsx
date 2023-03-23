8591371af9d16f047ab7fa487a0f2dff06fecdf3
Tatiana <taty07@gmail.com>
2023-03-23 10:30:05 +0100

    reviews done

--- 04-fundamental-projects/03-reviews/starter/src/Alternative.jsx
+++ 04-fundamental-projects/03-reviews/starter/src/Alternative.jsx
@@ -1,41 +1,34 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
-
 const App = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
 
-  const checkNumber = (number) => {
-    if (number > people.length - 1) {
-      return 0;
-    }
-    if (number < 0) {
-      return people.length - 1;
-    }
-    return number;
-  };
-
   const nextPerson = () => {
-    setIndex((currentIndex) => {
-      const newIndex = currentIndex + 1;
-      return checkNumber(newIndex);
+    setIndex((index) => {
+      // let newIndex = index + 1;
+      // console.log(newIndex);
+      // console.log(newIndex % people.length);
+      const newValue = (index + 1) % people.length;
+      console.log(newValue);
+      return newValue;
     });
   };
-
   const prevPerson = () => {
-    setIndex((currentIndex) => {
-      const newIndex = currentIndex - 1;
-      return checkNumber(newIndex);
+    setIndex((index) => {
+      const newValue = (index - 1 + people.length) % people.length;
+      return newValue;
     });
   };
-
   const randomPerson = () => {
     let randomNumber = Math.floor(Math.random() * people.length);
     if (randomNumber === index) {
       randomNumber = index + 1;
     }
-    setIndex(checkNumber(randomNumber));
+    const newIndex = randomNumber % people.length;
+    // console.log(randomNumber, newIndex);
+    setIndex(newIndex);
   };
 
   return (
@@ -51,15 +44,15 @@ const App = () => {
         <p className="job">{job}</p>
         <p className="info">{text}</p>
         <div className="btn-container">
-          <button type="button" className="prev-btn" onClick={prevPerson}>
+          <button className="prev-btn" onClick={prevPerson}>
             <FaChevronLeft />
           </button>
-          <button type="button" className="next-btn" onClick={nextPerson}>
+          <button className="next-btn" onClick={nextPerson}>
             <FaChevronRight />
           </button>
         </div>
         <button className="btn btn-hipster" onClick={randomPerson}>
-          Surprise me
+          surprise me
         </button>
       </article>
     </main>
