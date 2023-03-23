bcf2960d1eff23affcbdc2b8112c930eeed7a832
Tatiana <taty07@gmail.com>
2023-03-23 11:36:50 +0100

    changes done

--- 04-fundamental-projects/04-accordion/starter/src/SingleQuestion.jsx
+++ 04-fundamental-projects/04-accordion/starter/src/SingleQuestion.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+
 const SingleQuestion = ({ title, info }) => {
   const [showInfo, setShowInfo] = useState(false);
   return (
