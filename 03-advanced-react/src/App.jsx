d0b4b93ba5b379dc533c2efada685ff28cb7cab7
Tatiana <taty07@gmail.com>
2023-03-27 22:14:45 +0200

    forms finished

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,5 +1,5 @@
 // import { Home, About } from './tutorial/04-project-structure/starter/Pages';
-import Starter from './tutorial/06-forms/starter/02-user-challenge';
+import Starter from './tutorial/06-forms/starter/05-form-data';
 
 function App() {
   return (
